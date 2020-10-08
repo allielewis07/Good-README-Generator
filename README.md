78af0c181e3c52b1f8e8267d94bcf703fef927a9
allie Lewis <allielewis07@gmail.com>
2020-10-08 15:33:18 -0400

    modified README

--- README.md
+++ README.md
@@ -1,27 +1,25 @@
-# Good-READEME-Generator
-    # Welcome to READEME Generator: 
+# GOOD-README-Generator
+    # Welcome to GOOD-README-Generator: 
     ## Description
       undefined
     ## Table of Contents
     Navigate through the README Using the Table of Contents : 
-      * [Installation](#installation)
-      * [Usage](#usage)
-      * [License](#license)
-      * [Contributions](#contributing)
-      * [Badges](#badges)
-      * [Tests](#tests)
-      * [Questions](#questions)
-    ## Installation
-      undefined
-    ## Usage
-      undefined
-    ## License
-      undefined
-    ## Contributing
-      undefined
-    ## Badges
-    ## Tests
-      undefined
-    ## Questions
-    [Link to Creator Github](https://github.com/tght)
-    
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributions](#contributing)
+* [Badges](#badges)
+* [Tests](#tests)
+* [Questions](#questions)
+## Installation undefined
+## Usage
+undefined
+## License
+undefined
+## Contributing
+  undefined
+## Badges
+## Tests
+  undefined
+## Questions
+[Link to Creator Github](https://github.com/allielewis07)
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -6,26 +6,25 @@ function generateMarkdown(data) {
       ${data.description}
     ## Table of Contents
     Navigate through the README Using the Table of Contents : 
-      * [Installation](#installation)
-      * [Usage](#usage)
-      * [License](#license)
-      * [Contributions](#contributing)
-      * [Badges](#badges)
-      * [Tests](#tests)
-      * [Questions](#questions)
-    ## Installation
-      ${data.instructions}
-    ## Usage
-      ${data.usage}
-    ## License
-      ${data.license}
-    ## Contributing
-      ${data.contributors}
-    ## Badges
-    ## Tests
-      ${data.tests}
-    ## Questions
-    [Link to Creator Github](https://github.com/${data.gitUserName})
-    `)
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributions](#contributing)
+* [Badges](#badges)
+* [Tests](#tests)
+* [Questions](#questions)
+## Installation ${data.instructions}
+## Usage
+${data.usage}
+## License
+${data.license}
+## Contributing
+  ${data.contributors}
+## Badges
+## Tests
+  ${data.tests}
+## Questions
+[Link to Creator Github](https://github.com/${data.gitUserName})
+`)
 }
 module.exports = generateMarkdown;
